5e5a7b7ff26c10e46ae69b751ba015d0c964e069
rowanabisaiutp <a.rowan.ojeda@utponiente.edu.mx>
2025-10-06 08:43:58 -0600

    settings: PORT docker

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permitir conexiones externas
+    port: 5173,      // Puerto del contenedor
+    watch: {
+      usePolling: true, // Necesario para Docker en Windows
+    },
+  },
 })
